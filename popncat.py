89940125ac86b89c65d1ed858aaebfa7b7839748
cha0yunghuang <eric@erysia.com>
2022-04-12 11:29:56 +0800

    main function done

---
+++ popncat.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Explicit-Waits
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Chrome('/Users/erichuang/Dev/chromedriver')
+# direct to...
+driver.get('https://popcat.click/')
+
+# select item
+cat = driver.find_element_by_xpath('//*[@id="app"]/div')
+
+# explicit-wait for the cat appear
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div'))
+)
+
+actions = ActionChains(driver)
+
+for i in range(10000):
+    actions.click(cat).perform()  # single-click
+    actions.double_click(cat).perform()  # double-click
+
+driver.close()
+    
